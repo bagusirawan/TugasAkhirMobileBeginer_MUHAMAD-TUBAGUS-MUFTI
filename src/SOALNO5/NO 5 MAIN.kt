c37fad0cd535bb0c5e64935012b3a4bbf9c2abe9
bagusirawan <bagusurawan1234@gmail.com>
2024-08-01 20:20:30 +0700

    first commit

---
+++ src/SOALNO5/NO 5 MAIN.kt
@@ -0,0 +1,7 @@
+package SOALNO5
+
+// Main.kt
+fun main() {
+    val mahasiswa = Soal5("Bagus Mufti", 19, "Politeknik Harapan Bersama", "2A", "main game")
+    mahasiswa.datadiri()
+}
---
+++ src/SOALNO5/NO 5.kt
@@ -0,0 +1,6 @@
+package SOALNO5
+class Soal5(val nama: String, val umur: Int, val plt: String, val kelas: String, val hobi: String) {
+    fun datadiri() {
+        println("Nama saya adalah $nama, Saya adalah mahasiswa $plt kelas $kelas, Hobi saya $hobi.")
+    }
+}
